4dd7f1bedf5098e9fee759df24fc1e0e5733dd18
Muhammed Tariq <momotrq94@gmail.com>
2022-04-13 15:14:53 -0400

    created client.js to hold connect function and used ES6 shorthand to export it

---
+++ client.js
@@ -0,0 +1,21 @@
+//connect function
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
